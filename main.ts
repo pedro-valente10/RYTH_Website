f0b94f10f9ad6a823526dd38cd358d2fe007b9d9
Pedro Henrique Padilha Valente <pedrohenriquepvalente@gmail.com>
2026-09-21 18:11:02 -0300

    index corrigido

---
+++ main.ts
@@ -0,0 +1,5 @@
+import "./src/styles/reset.css";
+import "./src/styles/global.css";
+import "./src/styles/variables.css";
+
+console.log("RYTH iniciado!");
--- src/main.ts
+++
@@ -1,4 +0,0 @@
-import "./styles/reset.css";
-import "./styles/global.css";
-
-console.log("RYTH iniciado!");
