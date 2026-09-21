a37f98ad4214f30057529f3e46b966f6fd2e08e3
Pedro Henrique Padilha Valente <pedrohenriquepvalente@gmail.com>
2026-09-21 18:17:22 -0300

    vite configurado

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    port: 3000
+  // Diz ao Vite que a nova raiz do projeto é a pasta pages
+  root: 'src/pages/home',
+
+  build: {
+    // Como a raiz mudou, a pasta final de produção (dist) iria parar dentro de src/pages.
+    // Isso garante que o build continue sendo gerado na raiz original do projeto.
+    outDir: '../../../dist',
+    emptyOutDir: true 
   }
 });
