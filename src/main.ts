19067117a22ca9f15ac7da87bdb56457a9eb54bf
Pedro Henrique Padilha Valente <pedrohenriquepvalente@gmail.com>
2026-09-23 19:38:33 -0300

    update index

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,25 @@ import "./components/footer/footer.css";
 import { renderNavbar } from "./components/navbar/navbar";
 import { renderFooter } from "./components/footer/footer";
 
+function markActiveLink(): void {
+  const normalize = (path: string): string =>
+    path.replace(/\/index\.html$/, '/').replace(/\/$/, '') || '/';
+
+  const current = normalize(location.pathname);
+
+  document
+    .querySelectorAll<HTMLAnchorElement>('.navbar__link a')
+    .forEach((link) => {
+      const href = normalize(new URL(link.href, location.origin).pathname);
+
+      if (href === current) {
+        link.setAttribute('aria-current', 'page');
+      } else {
+        link.removeAttribute('aria-current');
+      }
+    });
+}
+
 renderNavbar();
+markActiveLink();
 renderFooter();
